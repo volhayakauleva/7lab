00db739e707d28cb234c5c927a2dd94e7053274e
volhayakauleva <52138253+volhayakauleva@users.noreply.github.com>
2020-06-12 21:54:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,127 +1,140 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Lab7
 {
-    class Program
+    class RationalNumber : IComparable
     {
-        class RationalNumber : IComparable
+        private int N;
+        private int M;
+        public char[] sep = {'/', ':' };
+        
+        public RationalNumber()
         {
-            private int N;
-            private int M;
-            public char[] sep = { ' ', '/', ':' };
-            
-            public RationalNumber()
+            N = 0;
+            M = 0;
+        }
+        
+        public RationalNumber(int n, int m)
+        {
+            N = n;
+            M = m;
+        }
+        
+        public RationalNumber(string s)
+        {
+            s = s.Trim();        
+            string reg = @"\d+[/-:]\d+$";
+            while (true)
             {
-                N = 0;
-                M = 0;
+                if (Regex.IsMatch(s, reg))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод, введите строку снова");
+                    s = Console.ReadLine();
+                }
             }
-            
-            public RationalNumber(int n, int m)
-            {
-                N = n;
-                M = m;
-            }
-            
-            public RationalNumber(string s)
-            {
-                s = s.Trim();
-                while (s.Contains("  "))
-                    s = s.Replace("  ", " ");
-                string[] result = s.Split(sep);
-                N = Convert.ToInt32(result[0]);
-                M = Convert.ToInt32(result[1]);
-            }
-
-            public void Print()
-            {
-                Console.WriteLine(N + "/" + M);
-            }
-            public override string ToString()
-            {
-                return N + "/" + M;
-            }
-
-            public static implicit operator int(RationalNumber rationalNumber)
-            {
-                return rationalNumber.N / rationalNumber.M;
-            }
-
-            public static explicit operator double(RationalNumber rationalNumber)
-            {
-                return (double)rationalNumber.N / (double)rationalNumber.M;
-            }
-
-            public static RationalNumber operator +(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                RationalNumber result = new RationalNumber();
-                result.N = (rationalNumber1.N * rationalNumber2.M) + (rationalNumber2.N * rationalNumber1.M);
-                result.M = rationalNumber1.M * rationalNumber2.M;
-                return result;
-            }
-
-            public static RationalNumber operator -(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                RationalNumber result = new RationalNumber();
-                result.N = (rationalNumber1.N * rationalNumber2.M) - (rationalNumber2.N * rationalNumber1.M);
-                result.M = rationalNumber1.M * rationalNumber2.M;
-                return result;
-            }
-
-            public static RationalNumber operator *(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                RationalNumber result = new RationalNumber();
-                result.N = rationalNumber1.N * rationalNumber2.N;
-                result.M = rationalNumber1.M * rationalNumber2.M;
-                return result;
-            }
-
-            public static RationalNumber operator /(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                RationalNumber result = new RationalNumber();
-                result.N = rationalNumber1.N * rationalNumber2.M;
-                result.M = rationalNumber1.M * rationalNumber2.N;
-                return result;
-            }
-
-            public int CompareTo(object obj)
-            {
-                RationalNumber rationalNum = obj as RationalNumber;
-                if ((this.N * rationalNum.M) < (rationalNum.N * this.M))
-                    return -1;
-                if ((this.N * rationalNum.M) > (rationalNum.N * this.M))
-                    return 1;
-                else return 0; 
-            }
-
-            public static bool operator <(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                return rationalNumber1.CompareTo(rationalNumber2) < 0;
-            }
-            
-            public static bool operator >(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                return rationalNumber1.CompareTo(rationalNumber2) > 0;
-            }
-            
-            public static bool operator !=(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                return rationalNumber1.CompareTo(rationalNumber2) != 0;
-            }
-            
-            public static bool operator ==(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
-            {
-                return rationalNumber1.CompareTo(rationalNumber2) == 0;
-            }
+            string[] result = s.Split(sep);
+            N = Convert.ToInt32(result[0]);
+            M = Convert.ToInt32(result[1]);
         }
 
+        public void Print()
+        {
+            Console.WriteLine(N + "/" + M);
+        }
+        
+        public override string ToString()
+        {
+            return N + "/" + M;
+        }
+
+        public static implicit operator int(RationalNumber rationalNumber)
+        {
+            return rationalNumber.N / rationalNumber.M;
+        }
+
+        public static explicit operator double(RationalNumber rationalNumber)
+        {
+            return (double)rationalNumber.N / (double)rationalNumber.M;
+        }
+
+        public static RationalNumber operator +(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            RationalNumber result = new RationalNumber();
+            result.N = (rationalNumber1.N * rationalNumber2.M) + (rationalNumber2.N * rationalNumber1.M);
+            result.M = rationalNumber1.M * rationalNumber2.M;
+            return result;
+        }
+
+        public static RationalNumber operator -(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            RationalNumber result = new RationalNumber();
+            result.N = (rationalNumber1.N * rationalNumber2.M) - (rationalNumber2.N * rationalNumber1.M);
+            result.M = rationalNumber1.M * rationalNumber2.M;
+            return result;
+        }
+
+        public static RationalNumber operator *(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            RationalNumber result = new RationalNumber();
+            result.N = rationalNumber1.N * rationalNumber2.N;
+            result.M = rationalNumber1.M * rationalNumber2.M;
+            return result;
+        }
+
+        public static RationalNumber operator /(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            RationalNumber result = new RationalNumber();
+            result.N = rationalNumber1.N * rationalNumber2.M;
+            result.M = rationalNumber1.M * rationalNumber2.N;
+            return result;
+        }
+
+        public int CompareTo(object obj)
+        {
+            RationalNumber rationalNum = obj as RationalNumber;
+            if ((this.N * rationalNum.M) < (rationalNum.N * this.M))
+                return -1;
+            if ((this.N * rationalNum.M) > (rationalNum.N * this.M))
+                return 1;
+            else return 0;
+        }
+
+        public static bool operator <(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return rationalNumber1.CompareTo(rationalNumber2) < 0;
+        }
+        
+        public static bool operator >(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return rationalNumber1.CompareTo(rationalNumber2) > 0;
+        }
+        
+        public static bool operator !=(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return rationalNumber1.CompareTo(rationalNumber2) != 0;
+        }
+        
+        public static bool operator ==(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return rationalNumber1.CompareTo(rationalNumber2) == 0;
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
             RationalNumber rationalNumber1 = new RationalNumber(1, 4);
-            RationalNumber rationalNumber2 = new RationalNumber(" 20/3");
+            RationalNumber rationalNumber2 = new RationalNumber(Console.ReadLine());
             rationalNumber1.Print();
             Console.WriteLine(rationalNumber2.ToString());
             int intValue = rationalNumber1;
